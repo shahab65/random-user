0c42563735b6943a85e51ff281f3c89a20c3c18a
shahab <shahab.emami95@gmail.com>
2022-07-11 03:28:25 +0430

    adding github deploy config

--- src/components/CardFooter/index.tsx
+++ src/components/CardFooter/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "components/Button";
 import styles from "./style.module.css";
-import CircularProgress from "@mui/material/CircularProgress";
 import Loading from "components/Loading";
 
 type Props = {
